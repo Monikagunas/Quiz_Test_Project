141a87872624607afccaaf98330755fea6e178b9
monikagunas <monikaguna2000@gmail.com>
2024-07-15 20:30:12 +0530

    Comment Added

--- SpringQuizDB/src/main/java/Controller/QuizController.java
+++ SpringQuizDB/src/main/java/Controller/QuizController.java
@@ -1,5 +1,5 @@
 package Controller;
-
+//Create controller
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
